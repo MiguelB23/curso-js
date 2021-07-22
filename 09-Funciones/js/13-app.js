a28863537e641010e45bfaa098e3eedc8948aa65
MiguelB <miguelbruno996@gmail.com>
2021-07-22 18:06:28 -0300

    Terminé todas las clases de Functions - 18:05

--- 09-Funciones/js/13-app.js
+++ 09-Funciones/js/13-app.js
@@ -2,17 +2,28 @@ const reproductor = {
     reproducir: id => console.log(`Reproduciendo canción con el id ${id}`),
     pausar: () => console.log('pausando...'),
     borrar: id => console.log(`Borrando canción... ${id}`),
-    reproducirPy: id => console.log(`Estás escuchando tu lista ${id}`)
+    agregarPlaylist: id => console.log(`canción agregada a la Lista de Reproducción ${id}`),
+    reproducirPy: id => console.log(`Estás escuchando tu lista ${id}`),
+
+    set newSong(song) {
+        this.song = song;
+        console.log(`Añadiendo ${song}`);
+    },
+    get getSong() {
+        console.log(`${this.song}`)
+    } 
 }
+
+reproductor.newSong = "Perfidia de Los Panchos";
+reproductor.getSong;
+
+//Esto de GET y SET no lo entiendo muy bien.
 
 reproductor.reproducir(30);
 reproductor.reproducir(20);
 reproductor.pausar();
 reproductor.borrar(21);
 
-reproductor.agregarPlaylist = function (id) {
-    console.log(`canción agregada a la Lista de Reproducción ${id}`);
-}
 
 reproductor.agregarPlaylist("Mejores canciones");
 reproductor.reproducirPy("Mejores Canciones");
